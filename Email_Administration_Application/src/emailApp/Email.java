f6b10b30999e254bfc674d32a5b49b81142b12a8
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-08-29 13:59:50 +0530

    neccessary informations

--- Email_Administration_Application/src/emailApp/Email.java
+++ Email_Administration_Application/src/emailApp/Email.java
@@ -1,6 +1,21 @@
 package emailApp;
 
 public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int emailboxCapacity;
+	private String alternateEmail;
 	
+	//Constructor for receiving first name and last name
+	
+	//Asking for Department
+	
+	//To Generate a random password
+	
+	//Set a Alternate Email
+	
+	//Change the Password
 
 }
