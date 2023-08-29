59bea5ce0f848602cb4326f32ff48cc9c951eb3b
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2023-08-29 14:51:11 +0530

    Getting Department name using a Method created

--- Email_Administration_Application/src/emailApp/Email.java
+++ Email_Administration_Application/src/emailApp/Email.java
@@ -1,5 +1,7 @@
 package emailApp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
@@ -12,9 +14,30 @@ public class Email {
 	public Email(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+		
+		//Calling a Method for asking  the department - return the Department
+		this.department = getDepartment();
 	}
 	
 	//Asking for Department
+	private String getDepartment() {
+		System.out.println("Department Codes: "
+				+ "\nA09f - Sales"
+				+ "\nC35o - Dev"
+				+ "\nV45y - Acc"
+				+ "\nNA - none"
+				+ "\nEnter Your Department Code: ");
+		Scanner sc = new Scanner(System.in);
+		String deptCode = sc.next();
+		switch (deptCode) {
+		case "A09f": return "Sales";
+		case "C35o": return "Development";
+		case "V45y": return "Accounting";
+		case "": return "";
+		default:
+			throw new IllegalArgumentException("Unexcepted Value Enter =>"+deptCode);
+		}
+	}
 	
 	//To Generate a random password
 	
